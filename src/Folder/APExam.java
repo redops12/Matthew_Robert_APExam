4c0c6aa0a187cce464b842597e09c2f6e554b983
Student <student@HURimcL130232bd.local>
2017-04-19 08:50:45 -0400

    combined code

--- src/Folder/APExam.java
+++ src/Folder/APExam.java
@@ -1,9 +1,15 @@
 package Folder;
 
 import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Scanner;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class APExam extends Canvas {
     
@@ -11,6 +17,12 @@ public class APExam extends Canvas {
     static int height = 144;
     static int scale = 3;
     double answer, comp;
+    
+    private JPanel window = new JPanel();
+	private JButton one, two, three, four, five, six, seven, eight, nine;
+	private Rectangle rone, rtwo, rthree, rfour, rfive, rsix, rseven, reight, rnine;
+	protected int buttonWidth = 60;
+	protected int buttonHeight = 30;
     
 	private void thing(double x, double y) {
     	if (comp == 1) {
@@ -95,10 +107,44 @@ public class APExam extends Canvas {
     	answer = a;
     	return answer;
 	}
+	
+	private void drawButtons(String name, JButton button, Rectangle rect, int x, int y, final int id) {
+    	button = new JButton(name);
+        rect = new Rectangle(x, y, buttonWidth, buttonHeight);
+        button.setBounds(rect);
+        window.add(button);
+        
+        button.addActionListener(new ActionListener() {
+        		public void actionPerformed(ActionEvent e) {
+        			if (id == 1) {
+        				System.out.println("Button One!");
+        				
+        			} else if (id == 2) {
+        				System.out.println("Button Two!");
+        				
+        			} else if (id == 3) {
+        				System.out.println("Button Three!");
+        				
+        			} else if (id == 4) {
+        				System.out.println("Button Four!");
+        				
+        			} else if (id == 5) {
+        				System.out.println("Button Five!");
+        				
+        			} else if (id == 5) {
+        				System.out.println("Button Five!");
+        				
+        			} else if (id == 5) {
+        				System.out.println("Button Five!");
+        				
+        			}
+        		}
+        });
+        
+    }
     
 	public static void main(String[] args) {
     	APExam main = new APExam();
-    	JFrame frame = new JFrame();
     	Scanner scanner = new Scanner(System.in);
     	double x, y;
     	x = scanner.nextDouble();
@@ -106,11 +152,23 @@ public class APExam extends Canvas {
     	main.comp = scanner.nextDouble();
     	//String comp;
    	 
-    	frame.add(main);
-    	frame.setResizable(false);
-    	frame.setLocationRelativeTo(null);
-    	frame.setSize(width * scale, height * scale);
-    	frame.pack();
+    	JFrame frame = new JFrame();//original code named this "FrameDemo" as a string parameter
+        frame.setPreferredSize(new Dimension(width * scale, height * scale));//not in original code, used to set size
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(main.window);
+        frame.pack();
+        frame.setVisible(true);
+        main.window.setLayout(null);
+        
+        main.drawButtons("1", main.one, main.rone, 12, 12, 1);
+        main.drawButtons("2", main.two, main.rtwo, 12, 54, 2);
+        main.drawButtons("3", main.three, main.rthree, 12, 96, 3);
+        main.drawButtons("4", main.four, main.rfour, 84, 12, 4);
+        main.drawButtons("5", main.five, main.rfive, 84, 54, 5);
+        main.drawButtons("6", main.six, main.rsix, 84, 96, 6);
+        main.drawButtons("7", main.seven, main.rseven, 156, 12, 7);
+        main.drawButtons("8", main.eight, main.reight, 156, 54, 8);
+        main.drawButtons("9", main.nine, main.rnine, 156, 96, 9);
    	 
     	main.thing(x, y);
 	}
